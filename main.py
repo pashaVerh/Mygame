dad8bd0e7a503c2391a800305bbc8de33fc3add6
pashaVerh <pasha.verhovsky@yandex.ru>
2022-05-01 13:46:24 +0300

    я впервые попробовал гит

---
+++ main.py
@@ -0,0 +1,136 @@
+# Импортируем библиотеку pygame
+import pygame
+from pygame import *
+from player import Player, Treasure
+from platfrom import Platform
+
+# Объявляем переменные
+WIN_WIDTH = 800  # Ширина создаваемого окна
+WIN_HEIGHT = 640  # Высота
+DISPLAY = (WIN_WIDTH, WIN_HEIGHT)  # Группируем ширину и высоту в одну переменную
+BACKGROUND_COLOR = "#004400"
+PLATFORM_WIDTH = 32
+PLATFORM_HEIGHT = 32
+PLATFORM_COLOR = "#FF6262"
+
+level_1 = [
+    "-------------------------",
+    "-                       -",
+    "-                       -",
+    "-                   *   -",
+    "-               ---------",
+    "-                       -",
+    "-    -      --          -",
+    "-                       -",
+    "--                      -",
+    "-   --                  -",
+    "-                   --- -",
+    "-                       -",
+    "-                       -",
+    "-      ---              -",
+    "-                       -",
+    "-   -----------         -",
+    "-                       -",
+    "- --                --  -",
+    "-                       -",
+    "-------------------------"]
+
+level_2 = [
+    "-------------------------",
+    "-                       -",
+    "-                       -",
+    "--              ---------",
+    "-                       -",
+    "-                      *-",
+    "-      --       ------ --",
+    "-                       -",
+    "-                       -",
+    "-   --     -            -",
+    "-                   --- -",
+    "-                       -",
+    "-                       -",
+    "-    --   --     --     -",
+    "-                       -",
+    "-      -----------      -",
+    "-                       -",
+    "-                   --  -",
+    "-                       -",
+    "-------------------------"]
+
+
+def level_loader(level, hero):
+    tr = None
+    entities = pygame.sprite.Group()  # Все объекты
+    platforms = []  # то, во что мы будем врезаться или опираться
+    entities.add(hero)
+    x = y = 0  # координаты
+    for row in level:  # вся строка
+        for col in row:  # каждый символ
+            if col == "-":
+                pf = Platform(x, y)
+                entities.add(pf)
+                platforms.append(pf)
+            elif col == '*':
+                tr = Treasure(x, y)
+                entities.add(tr)
+            x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
+        y += PLATFORM_HEIGHT  # то же самое и с высотой
+        x = 0  # на каждой новой строчке начинаем с нуля
+    return entities, platforms, tr
+
+
+timer = pygame.time.Clock()
+
+
+def main():
+    pygame.init()  # Инициация PyGame, обязательная строчка
+    screen = pygame.display.set_mode(DISPLAY)  # Создаем окошко
+    pygame.display.set_caption("My First Game")  # Пишем в шапку
+    bg = Surface(DISPLAY)  # Создание видимой поверхности background
+
+    # будем использовать как фон
+    bg.fill(Color(BACKGROUND_COLOR))  # Заливаем поверхность сплошным цветом
+
+    hero = Player(55, 55)  # создаем героя по (x,y) координатам
+    left = right = up = False  # по умолчанию — стоим
+
+    entities, platforms, tr = level_loader(level_1, hero)
+
+    while 1:  # Основной цикл программы
+        timer.tick(60)
+        if pygame.sprite.collide_rect(hero, tr):
+            entities, platforms, tr = level_loader(level_2, hero)
+        # _____________EVENTS_____________
+        for e in pygame.event.get():  # Обрабатываем события
+            if e.type == QUIT:
+                exit(0)
+
+            if e.type == KEYDOWN:
+                if e.key == K_LEFT:
+                    left = True
+                if e.key == K_RIGHT:
+                    right = True
+                if e.key == K_UP:
+                    up = True
+
+            if e.type == KEYUP:
+                if e.key == K_RIGHT:
+                    right = False
+                if e.key == K_LEFT:
+                    left = False
+                if e.key == K_UP:
+                    up = False
+
+        # _____________DRAWING_____________
+
+        screen.blit(bg, (0, 0))  # Каждую итерацию необходимо всё перерисовывать
+
+        hero.update(left, right, up, platforms)
+
+        entities.draw(screen)
+
+        pygame.display.update()  # обновление и вывод всех изменений на экран
+
+
+if __name__ == "__main__":
+    main()
---
+++ player.py
@@ -0,0 +1,79 @@
+import os.path
+
+from pygame import *
+MOVE_SPEED = 5
+WIDTH = 22
+HEIGHT = 32
+COLOR = "#888888"
+treasure_color = '#f4f72f'
+JUMP_POWER = 10
+GRAVITY = 0.35  # Сила, которая будет тянуть нас вниз
+
+
+class Player(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.xvel = 0  # скорость перемещения. 0 - стоять на месте
+        self.startX = x  # Начальная позиция Х, пригодится когда будем переигрывать уровень
+        self.startY = y
+        self.image = C:\Users\15\PycharmProjects\PYGAME\img.png
+        self.image = transform.scale(25, 35)
+        self.rect = Rect(x, y, WIDTH, HEIGHT)  # прямоугольный объект
+        self.yvel = 0  # скорость вертикального перемещения
+        self.on_ground = False  # На земле ли я?
+
+    def update(self, left, right, up, platforms):
+        if left:
+            self.xvel = -MOVE_SPEED  # Лево = x- n
+
+        if right:
+            self.xvel = MOVE_SPEED  # Право = x + n
+
+        if not (left or right):  # стоим, когда нет указаний идти
+            self.xvel = 0
+
+        if up:
+            if self.on_ground:  # прыгаем, только когда можем оттолкнуться от земли
+                self.yvel = -JUMP_POWER
+
+        if not self.on_ground:
+            self.yvel += GRAVITY
+
+        self.on_ground = False  # Мы не знаем, когда мы на земле((
+
+        self.rect.y += self.yvel
+        self.collide(0, self.yvel, platforms)
+
+        self.rect.x += self.xvel  # переносим свои положение на xvel
+        self.collide(self.xvel, 0, platforms)
+
+    def draw(self, screen):  # Выводим себя на экран
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    def collide(self, xvel, yvel, platforms):
+        for p in platforms:
+            if sprite.collide_rect(self, p):  # если есть пересечение платформы с игроком
+
+                if xvel > 0:  # если движется вправо
+                    self.rect.right = p.rect.left  # то не движется вправо
+
+                if xvel < 0:  # если движется влево
+                    self.rect.left = p.rect.right  # то не движется влево
+
+                if yvel > 0:  # если падает вниз
+                    self.rect.bottom = p.rect.top  # то не падает вниз
+                    self.on_ground = True  # и становится на что-то твердое
+                    self.yvel = 0  # и энергия падения пропадает
+
+                if yvel < 0:  # если движется вверх
+                    self.rect.top = p.rect.bottom  # то не движется вверх
+                    self.yvel = 0  # и энергия прыжка пропадает
+
+
+class Treasure(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((WIDTH, HEIGHT))
+        self.image.fill(Color(treasure_color))
+        self.rect = Rect(x, y, WIDTH, HEIGHT)
+
