d4ccef58ecb06dd78a5bc2ff7dd3d248645804d1
pashaVerh <pasha.verhovsky@yandex.ru>
2022-05-22 19:11:13 +0300

    я впервые попробовал гит

--- platfrom.py
+++ platfrom.py
@@ -1,4 +1,6 @@
+import pygame.image
 from pygame import *
+
 
 PLATFORM_WIDTH = 32
 PLATFORM_HEIGHT = 32
@@ -8,6 +10,6 @@ PLATFORM_COLOR = "#FF6262"
 class Platform(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
-        self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-        self.image.fill(Color(PLATFORM_COLOR))
+        self.image = pygame.image.load("images/img.jpg")
+        self.image = transform.scale(self.image, (35, 35))
         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
